1561ae6d3618345e5c7f1aa0a789508e11588875
vinu <vinu@xminds.com>
2020-06-08 12:48:14 +0530

    Use spring kafka prop then override the necessary

--- kafka-spring-boot-autoconfigure/src/main/java/rocks/vinu/kafkaautoconfigure/RockingKafkaAutoConfiguration.java
+++ kafka-spring-boot-autoconfigure/src/main/java/rocks/vinu/kafkaautoconfigure/RockingKafkaAutoConfiguration.java
@@ -1,32 +1,31 @@
 package rocks.vinu.kafkaautoconfigure;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.listener.ContainerProperties;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @Configuration(proxyBeanMethods = false)
 @AutoConfigureBefore(KafkaAutoConfiguration.class)
+@Slf4j
 public class RockingKafkaAutoConfiguration {
 
     @Bean
-    public ConsumerFactory<String, String> kafkaConsumerFactory(Environment env) {
-        Map<String, Object> config = new HashMap<>();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, env.getProperty("spring.kafka.bootstrap-servers"));
-        config.put(ConsumerConfig.GROUP_ID_CONFIG, env.getProperty("spring.kafka.consumer.group-id"));
+    public ConsumerFactory<String, String> kafkaConsumerFactory(KafkaProperties kafkaProperties) {
+        Map<String, Object> config = kafkaProperties.buildConsumerProperties();
         config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         config.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
@@ -48,19 +47,17 @@ public class RockingKafkaAutoConfiguration {
     }
 
     @Bean
-    public ProducerFactory<String, String> kafkaProducerFactory(Environment env) {
-        Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, env.getProperty("spring.kafka.bootstrap-servers"));
+    public ProducerFactory<String, String> kafkaProducerFactory(KafkaProperties kafkaProperties) {
+        Map<String, Object> config = kafkaProperties.buildProducerProperties();
         config.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, 1);
         config.put(ProducerConfig.ACKS_CONFIG, "all");
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        DefaultKafkaProducerFactory<String, String> factory = new DefaultKafkaProducerFactory<>(config);
-        return factory;
+        return new DefaultKafkaProducerFactory<>(config);
     }
 
     @Bean
-    KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> pdf){
-        return new KafkaTemplate<>(pdf);
+    KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> kafkaPf) {
+        return new KafkaTemplate<>(kafkaPf);
     }
 }
